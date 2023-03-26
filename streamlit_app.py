1e205855c9b535b130692b114bd0e295266b0c7f
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-26 12:54:34 -0400

    Update streamlit_app.py
    Adding the first items to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu') 
+streamlit.text('Omega 3 & Bleberry Oatmeal')
+streamli.text('Kale, Spinach & Rocket Smoothie')
+streamli.text('Har-Boiled Free-Range Egg')
+
