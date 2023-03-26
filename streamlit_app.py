a3e0a24d4717ecef2d5412302c07050dacefaec4
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-26 12:55:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu') 
 streamlit.text('Omega 3 & Bleberry Oatmeal')
-streamli.text('Kale, Spinach & Rocket Smoothie')
-streamli.text('Har-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Har-Boiled Free-Range Egg')
 
