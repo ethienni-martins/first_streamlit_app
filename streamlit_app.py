ee31634c312a9b521b4cd9d7e11cc65135cf114b
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-28 16:32:38 -0400

    Update streamlit_app.py
    Adding fruityvice responses (lesson 9, SnowFlake - Badge 2)

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list) // Changed 
 streamlit.dataframe(fruits_to_show)
 
-
+#New Section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
