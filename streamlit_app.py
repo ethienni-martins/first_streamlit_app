7e4d0e8f537301ec107f4ec8ddfa11e441636009
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-29 11:37:13 -0400

    Update streamlit_app.py
    Deleted the raw JSON presentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,11 @@ streamlit.text(fruityvice_response)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Writes data straight to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
+
+# streamlit.text(fruityvice_response.json()) # Writes data straight to the screen
+# Line above deleted/commented
+
 # Take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it to the screen as a table
