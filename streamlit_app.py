b3a9e0ffeaec11629662141f082f590d65e256c7
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-29 12:52:12 -0400

    Update streamlit_app.py
    Adding the fuit_choice variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,11 @@ streamlit.text(fruityvice_response)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would like information about?','Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # streamlit.text(fruityvice_response.json()) # Writes data straight to the screen
 # Line above deleted/commented
@@ -39,4 +42,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
 
