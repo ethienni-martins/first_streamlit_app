01cbc9818684c04ef803496a625fdad882fe8e6d
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-30 16:51:43 -0400

    Update streamlit_app.py
    Adding snowflake connector and Select statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,12 @@ fruit_choice = streamlit.text_input('What fruit would like information about?','
 streamlit.write('The user entered', fruit_choice)
 
 import requests
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # streamlit.text(fruityvice_response.json()) # Writes data straight to the screen
