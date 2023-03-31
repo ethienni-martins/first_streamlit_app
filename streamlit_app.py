8b459d70d90222d5ed7efa9cda927bcacf0da1e6
ethienni-martins <128940738+ethienni-martins@users.noreply.github.com>
2023-03-30 20:45:32 -0400

    Update streamlit_app.py
    Organizing the "IF" structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,24 +34,28 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 #New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
+#changes to the api response section
+try:
+  fruit_choice = streamlit.text_input('What fruit would like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please, select a fruit to receive information about.")
+  else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    # Take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Output it to the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+    
+  except URLError as e:
+    streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-# streamlit.text(fruityvice_response.json()) # Writes data straight to the screen
-# Line above deleted/commented
-
-# Take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+ streamlit.header('Fruityvice Fruit Advice')
+      
+ streamlit.write('The user entered', fruit_choice)
 
 
 #Stopping the app from adding rows to the fruit_load_list table
 streamlit.stop()
-
 
 # importing libraries
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
